5d303ebdd419b841e9ffbfcbb1d3c9e81335c911
ethaneanavim26-prog <ethaneanavim26@gmail.com>
2026-06-18 18:34:54 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
-import base44 from '@base44/vite-plugin'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
 export default defineConfig({
-  logLevel: 'error',
-  plugins: [
-    base44({
-      legacySDKImports: process.env.BASE44_LEGACY_SDK_IMPORTS === 'true',
-      hmrNotifier: true,
-      navigationNotifier: true,
-      analyticsTracker: true,
-      visualEditAgent: true
-    }),
-    react(),
-  ]
-});
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    }
+  }
+})
