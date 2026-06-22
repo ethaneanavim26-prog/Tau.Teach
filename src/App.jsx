af058fd5ca7a211f343c4e2d2f365ddee5437219
ethaneanavim26-prog <ethaneanavim26@gmail.com>
2026-06-22 09:14:53 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
 import { Toaster } from '@/components/ui/toaster'
-import Home from './pages/home'
+import Home from "./pages/Home.jsx";
 
 export default function App() {
   return (
